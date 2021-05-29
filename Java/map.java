173533f92ad344224be93f07958e480b750f5b9e
MindException <tourer97@naver.com>
2021-05-29 21:44:47 +0900

    지도 초기안 업데이트

--- Java/map.java
+++ Java/map.java
@@ -1,23 +1,123 @@
-package com.example.myapplication;
+package com.cookandroid.opensw_3team_cafereviewproject;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
-public class map extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-    private View view;
+import java.io.IOException;
+import java.util.List;
+
+
+public class map extends Fragment implements OnMapReadyCallback {
+
+    View view;
+    MapView mapView = null;
+    GoogleMap googleMap;
+    SupportMapFragment mapFragment;
+    SearchView searchView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.map, container, false);
 
-        return view;
+        mapView = view.findViewById(R.id.googlemap);
+/*
+        searchView = view.findViewById(R.id.sv_location);
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.googlemap);*/
+        mapView.getMapAsync(this);
+/*
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String location = searchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if (location != null || !location.equals("")) {
+                    Geocoder geocoder = new Geocoder(getActivity().getApplicationContext());
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+                    googleMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+        mapFragment.getMapAsync(this);*/
+
+        return view ;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if(mapView != null) {
+            mapView.onCreate(savedInstanceState);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+      //  createDeviceList();
+    }
+
+    @Override
+    public void onStop() {
+       // disconnectFunction();
+        mapView.onStop();
+        super.onStop();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        LatLng latLng = new LatLng(36.799769, 127.074831);
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,16));
+
     }
 }
